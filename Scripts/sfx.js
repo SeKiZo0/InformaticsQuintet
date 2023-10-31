de83e66144d5a179962fca40248cf47c07a3973a
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-10-31 21:39:24 +0200

    Added some formatting in the layout

--- Scripts/sfx.js
+++ Scripts/sfx.js
@@ -1,37 +1,113 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
-gsap.to(".blocks", {
-    scrollTrigger: {
-        trigger: ".blocks",
-        start: "top 100%",
-        end: "top 30%",
-        scrub: 4,
-        toggleActions : "restart none none none",
-        pin: true
+const Scroll = new function() {
+	let sections
+	let page
+	let main
+	let scrollTrigger
+	let tl
+	let win
+	
+	// Init
+	this.init = () => {
+		sections = document.querySelectorAll('.block')
+		page = document.querySelector('#blocks')
+		main = document.querySelector('main')
+		win = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		}
+		
+		this.setupTimeline()
+		this.setupScrollTrigger()
+		window.addEventListener('resize', this.onResize)
+	}
+	
+	// Setup ScrollTrigger
+	this.setupScrollTrigger = () => {
+		page.style.height = (this.getTotalScroll() + win.h) + 'px'
+		
+		scrollTrigger = ScrollTrigger.create({
+			id: 'mainScroll',
+			trigger: 'main',
+			animation: tl,
+			pin: true,
+			scrub: true,
+			snap: {
+				snapTo: (value) => {
+					
+					let labels = Object.values(tl.labels)
+					
+					const snapPoints = labels.map(x => x / tl.totalDuration());
+					const proximity = 0.1
+					
+					console.log(tl.labels , tl.totalDuration(), labels, snapPoints)
+					
+					for (let i = 0; i < snapPoints.length; i++) {
+						if (value > snapPoints[i] - proximity && value < snapPoints[i] + proximity) {
+							return snapPoints[i]
+						}
+					}
+				},
+				duration: { min: 0.2, max: 0.6 },
+			},
+			start: 'top top',
+			end: '+=' + this.getTotalScroll(),
+		})
+	}
+	
+	// Setup timeline
+	this.setupTimeline = () => {
+		tl = gsap.timeline()
+		tl.addLabel("label-initial")
+		
+		sections.forEach((section, index) => {
+			const nextSection = sections[index+1]
+			if (!nextSection) return
 
-    }
-})
+			tl.to(nextSection, {
+				y: -1 * nextSection.offsetHeight,
+				duration: nextSection.offsetHeight,
+				ease: 'linear',
+			})
+			.addLabel(`label${index}`)
+		})
+	}
+	
+	// On resize
+	this.onResize = () => {
+		win = {
+			w: window.innerWidth,
+			h: window.innerHeight
+		}
+		
+		this.reset()
+	}
+	
+	// Reset
+	this.reset = () => {
+		if (typeof ScrollTrigger.getById('mainScroll') === 'object') {
+			ScrollTrigger.getById('mainScroll').kill()
+		}
+		
+		if (typeof tl === 'object') {
+			tl.kill()
+			tl.seek(0)
+		}
+		
+		document.body.scrollTop = document.documentElement.scrollTop = 0
+		this.init()
+	}
+	
+	// Get total scroll
+	this.getTotalScroll = () => {
+		let totalScroll = 0
+		sections.forEach(section => {
+			totalScroll += section.offsetHeight
+		})
+		totalScroll -= win.h
+		return totalScroll
+	}
+}
 
-const tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: '.blocks',
-        pin: true,
-        start: 'top top',
-        end: 'bottom top',
-        scrub: 1,
-        ease: 'linear',
-      }
-  })
-
-  tl.to('.block .text', {
-    height: 0,
-    padding: 0,
-    opacity: 0,
-    stagger: .5,
-  })
-  tl.to('.block', {
-    marginBottom: 0,
-    paddingBottom: 0,
-    paddingTop: 0,
-    stagger: .5,
-  }, '<')
+Scroll.init()
