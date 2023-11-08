3fdb0b390225fcba394d8744d3d589e07c3f58d5
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-11-08 21:46:27 +0200

    going all cdn

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
-import { GLTFLoader } from "./Scripts/GLTFLoader";
+import { GLTFLoader } from "https://cdn.rawgit.com/mrdoob/three.js/master/examples/js/loaders/GLTFLoader.js";
 //sizes
 const sizes = {
     width: window.innerWidth,
