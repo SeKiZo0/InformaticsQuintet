6bec83b4f23765d5934d65dbb743ee77ef8b6d87
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-11-09 21:27:01 +0200

    Cut out alot of javascript but ended up with a lil more css :unamused:
    Took longer than it should've

--- Scripts/sfx.js
+++ Scripts/sfx.js
@@ -3,127 +3,25 @@
 window.onbeforeunload = function() {
 	window.scrollTo(0, 0);
 }*/
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 let speed = 500;
 
 // hills animation 
 //gsap.to("#mountain1", {scrollTrigger : { scrub : 1 }, y: 0.001 * speed})
 
-//Morris section
 
 
-//dont put animations below this point
-const Scroll = new function() {
-	let sections
-	let page
-	let main
-	let scrollTrigger
-	let tl
-	let win
-	
-	// Init
-	this.init = () => {
-		sections = document.querySelectorAll('.block')
-		page = document.querySelector('#blocks')
-		main = document.querySelector('main')
-		win = {
-			w: window.innerWidth,
-			h: window.innerHeight
-		}
-		
-		this.setupTimeline()
-		this.setupScrollTrigger()
-		window.addEventListener('resize', this.onResize)
-	}
-	
-	// Setup ScrollTrigger
-	this.setupScrollTrigger = () => {
-		page.style.height = (this.getTotalScroll() + win.h) + 'px'
-		
-		scrollTrigger = ScrollTrigger.create({
-			id: 'mainScroll',
-			trigger: 'main',
-			animation: tl,
-			pin: true,
-			scrub: true,
-			snap: {
-				snapTo: (value) => {
-					
-					let labels = Object.values(tl.labels)
-					
-					const snapPoints = labels.map(x => x / tl.totalDuration());
-					const proximity = 0.1
-					
-					console.log(tl.labels , tl.totalDuration(), labels, snapPoints)
-					
-					for (let i = 0; i < snapPoints.length; i++) {
-						if (value > snapPoints[i] - proximity && value < snapPoints[i] + proximity) {
-							return snapPoints[i]
-						}
-					}
-				},
-				duration: { min: 0.2, max: 0.6 },
-			},
-			start: 'top top',
-			end: '+=' + this.getTotalScroll(),
-		})
-	}
-	
-	// Setup timeline
-	this.setupTimeline = () => {
-		tl = gsap.timeline()
-		tl.addLabel("label-initial")
-		
-		sections.forEach((section, index) => {
-			const nextSection = sections[index+1]
-			if (!nextSection) return
+let panels = gsap.utils.toArray(".panels")
 
-			tl.to(nextSection, {
-				y: -1 * nextSection.offsetHeight,
-				duration: nextSection.offsetHeight,
-				ease: 'linear',
-			})
-			.addLabel(`label${index}`)
-		})
-	}
-	
-	// On resize
-	this.onResize = () => {
-		win = {
-			w: window.innerWidth,
-			h: window.innerHeight
-		}
-		
-		this.reset()
-	}
-	
-	// Reset
-	this.reset = () => {
-		if (typeof ScrollTrigger.getById('mainScroll') === 'object') {
-			ScrollTrigger.getById('mainScroll').kill()
-		}
-		
-		if (typeof tl === 'object') {
-			tl.kill()
-			tl.seek(0)
-		}
-		
-		document.body.scrollTop = document.documentElement.scrollTop = 0
-		this.init()
-	}
-	
-	// Get total scroll
-	this.getTotalScroll = () => {
-		let totalScroll = 0
-		sections.forEach(section => {
-			totalScroll += section.offsetHeight
-		})
-		totalScroll -= win.h
-		return totalScroll
-	}
+panels.forEach((panel, i) => {
+  
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top", 
+    pin: i === panels.length -1 ? false : true, 
+    end:"bottom ",
+    pinSpacing: false 
+  });
+});
 
-	
-}
-
-Scroll.init()
-
+//Morris section, everyone can put their stuff above
