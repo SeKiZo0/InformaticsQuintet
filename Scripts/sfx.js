93f19cefa8c0011a90622c1372e0ba6862fb50c1
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-11-15 19:11:07 +0200

    Tried adding some animations
    Have a issue where I have to resize the window to get it to work

--- Scripts/sfx.js
+++ Scripts/sfx.js
@@ -18,11 +18,32 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 let morrisAnimation = gsap.fromTo('.displacement',{attr: {r:0}},{attr: {r: 600},duration: 2});
+let morrisAnimation2 = gsap.fromTo('#morrisModal',{width: "99%"},{width:"100%",duration: 2});
 
 morrisAnimation.pause()
+morrisAnimation2.pause()
 
 let morrisBTN = document.getElementById('morrisA')
 
 morrisBTN.addEventListener('click', function() {
+  gsap.utils.toArray(".mContainer").forEach((container) => {
+
+    let morrisTl = gsap.timeline({
+      scrollTrigger: {
+        scroller: ".block5",
+        trigger: container,
+        start: "20% top",
+        scrub: false,
+        markers: true,
+        toggleActions:"play none none reverse"
+      }
+    })
+  
+  morrisTl.fromTo(container,{x:-1000, y:200},{x:500,y:200, duration: 2})
+  
+  })
   morrisAnimation.play()
+  morrisAnimation2.play()
+
 })
+
