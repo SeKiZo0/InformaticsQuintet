7a32032aefb98c88356550167114760d91607b11
SeKiZo0 <105446823+SeKiZo0@users.noreply.github.com>
2023-11-18 09:16:33 +0200

    Race animation done
    using a start similar to f1

--- Scripts/sfx.js
+++ Scripts/sfx.js
@@ -1,43 +1,71 @@
 //https://codepen.io/GreenSock/pen/WNzLeWX
 /*
 window.onbeforeunload = function() {
-	window.scrollTo(0, 0);
+  window.scrollTo(0, 0);
 //}*/
 gsap.registerPlugin(ScrollTrigger);
-//let speed = 500;
-//
-// hills animation 
-//gsap.to("#mountain1", {scrollTrigger : { scrub : 1 }, y: 0.001 * speed})
 
-
-
-
-//Morris section, everyone else can put their stuff above
-
-//This is when the animation starts, it initiates the process still neeed to fix the layers
-
-//let morrisAnimation = gsap.fromTo('.displacement',{attr: {r:0}},{attr: {r: 600},duration: 2});
-
-//morrisAnimation.pause()
-/*
-gsap.fromTo(".displacement", {
+let carA = gsap.timeline({
   scrollTrigger: {
-    scroller: ".block5",
-    trigger: ".mContent",
-    start: "top top",
-    scrub: true
-  },
-  attr: {r:0},
-  attr: {r: 600}
+    trigger: "#carContainer",
+    start: "top bottom",
+    scrub: false,
+    toggleActions: "restart pause resume pause",
+    markers: true
+  }
 })
-*/
+
+carA.fromTo("#car1",
+  //starting value, you can specify anything you want to change, keep in mind that this works similar to a transform animation
+  { x: -0 },
+  //ending value, here you will also have to specify duration and ease(https://gsap.com/docs/v3/Eases/)
+  { x: 2000, duration: 1, ease: "power3.in", }
+  //This is the position in the timeline specified in seconds(https://gsap.com/docs/v3/GSAP/Timeline/)
+  , 6)
+
+carA.fromTo("#car2",
+  { x: -0 },
+  { x: 2000, duration: 1, ease: "power3.in", },
+  6.1)
+
+carA.fromTo(".infoCard",
+//uses percent instead of px(https://gsap.com/community/forums/topic/8148-animating-x-with-percentage/#:~:text=GSAP%20can%20do%20percentage%20x,and%20yPercent%20for%20responsive%20animations.&text=Codepen%20example%20of%20interactive%20responsive,HTML)
+  { xPercent: -100 },
+  { xPercent: 0 },
+  7)
+var lightCounter = 1
+  gsap.utils.toArray(".light").forEach((light) => {
+    let lightsTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#carContainer",
+        start: "top bottom",
+        scrub: false,
+        toggleActions: "restart pause resume pause",
+        markers: true
+      }
+    })
+
+    lightsTl.fromTo(light,{backgroundColor: "grey"},{backgroundColor: "red", delay:lightCounter})
+    console.log(lightCounter += 1)
+    if (lightCounter == 6) {
+      lightsTl.fromTo(".light",{backgroundColor: "grey"},{backgroundColor: "green"})
+    }
+  })
+
+ 
+//Morris section, everyone else can put their stuff above
+//TODO I need to fix the rest of this
+
+let morrisAnimation = gsap.fromTo('.displacement',{attr: {r:0}},{attr: {r: 600},duration: 2});
+
+morrisAnimation.pause()
 
 let morrisBTN = document.getElementById('morrisA')
 
-morrisBTN.addEventListener('click', function() {
+morrisBTN.addEventListener('click', function () {
 
   gsap.utils.toArray(".mContainer").forEach((container) => {
-    
+
     let morrisTl = gsap.timeline({
       scrollTrigger: {
         scroller: ".block5",
@@ -45,15 +73,15 @@ morrisBTN.addEventListener('click', function() {
         start: "-100% top",
         scrub: false,
         //markers: true,
-        
+
       }
     })
-  
-  morrisTl.fromTo(container,{x:-50, opacity:0},{x:50, opacity:1, duration: 1})
-  
+
+    morrisTl.fromTo(container, { x: -50, opacity: 0 }, { x: 50, opacity: 1, duration: 1 })
+
   })
-  
-  //morrisAnimation.play()
+
+  morrisAnimation.play()
 })
 
 
@@ -61,31 +89,8 @@ var MorrisModal = new bootstrap.Modal(document.getElementById('morrisModal'));
 
 // Event listener for modal shown event
 MorrisModal._element.addEventListener('shown.bs.modal', function () {
-    // Your code to handle modal completion
-    ScrollTrigger.refresh();
+  // Your code to handle modal completion
+  // Refreshing scroll trigger
+  ScrollTrigger.refresh();
 });
-
-let carA = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#carContainer1",
-    start: "top bottom",
-    scrub: false,
-    toggleActions:"play none none reverse",
-    markers: true
-  }
-})
-
-carA.fromTo("#car1",{x:-50}, {x:2000, duration: 1, delay:5})
-
-let carB = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#carContainer2",
-    start: "top bottom",
-    scrub: false,
-    toggleActions:"play none none reverse",
-    markers: true
-  }
-})
-
-carB.fromTo("#car2",{x:-50}, {x:2000, duration: 1, delay:4.9})
 
